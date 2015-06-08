eed6287853d92c42524d1ff8423b942c6271df17
Thomas Crawford <tom@viznetwork.com>
2015-06-07 20:36:45 -0400

    Initial Creation of starter file for TableView class homework

--- iScream/AppDelegate.m
+++ iScream/AppDelegate.m
@@ -14,33 +14,55 @@
 
 @implementation AppDelegate
 
+NSString *const kModelDB = @"iScream.sqlite";
+NSString *const kModelDBWAL = @"iScream.sqlite-wal";
+NSString *const kModelDBSHM = @"iScream.sqlite-shm";
+NSString *const kModelDBResource = @"iScream";
+NSString *const kModelDBType     = @"sqlite";
+NSString *const kModelDBWALType  = @"sqlite-wal";
+NSString *const kModelDBSHMType  = @"sqlite-shm";
+
+#pragma mark - Database Methods
+
+- (NSString *) dbPath {
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    path = [[NSString pathWithComponents: [NSArray arrayWithObjects: path, kModelDB, nil]] stringByStandardizingPath];
+    return path;
+}
+
+- (BOOL) doesDBExistAtPath: (NSString *) path {
+    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath: path];
+    return fileExists;
+}
+
+- (void) copyDBToPath: (NSString *) path {
+    NSError *error = nil;
+    NSString *dbBundlePath	= [[NSBundle mainBundle] pathForResource: kModelDBResource ofType: kModelDBType];
+    NSString *dbWALBundlePath = [[NSBundle mainBundle] pathForResource: kModelDBResource ofType: kModelDBWALType];
+    NSString *dbSHMBundlePath = [[NSBundle mainBundle] pathForResource: kModelDBResource ofType: kModelDBSHMType];
+    if (dbBundlePath) {
+        [[NSFileManager defaultManager] copyItemAtPath: dbBundlePath toPath: path error: &error];
+        
+        NSString *destPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+        NSString *destPathWAL = [[NSString pathWithComponents: [NSArray arrayWithObjects: destPath, kModelDBWAL, nil]] stringByStandardizingPath];
+        [[NSFileManager defaultManager] copyItemAtPath: dbWALBundlePath toPath: destPathWAL error: &error];
+        
+        NSString *destPathSHM = [[NSString pathWithComponents: [NSArray arrayWithObjects: destPath, kModelDBSHM, nil]] stringByStandardizingPath];
+        [[NSFileManager defaultManager] copyItemAtPath: dbSHMBundlePath toPath: destPathSHM error: &error];
+    }
+}
+
+#pragma mark - App Life Cycle Methods
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    if (![self doesDBExistAtPath: self.dbPath]) {
+        NSLog(@"Copying");
+        [self copyDBToPath: self.dbPath];
+    }
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    // Saves changes in the application's managed object context before the application terminates.
     [self saveContext];
 }
 
@@ -51,12 +73,11 @@
 @synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
 
 - (NSURL *)applicationDocumentsDirectory {
-    // The directory the application uses to store the Core Data store file. This code uses a directory named "com.viznetwork.iScream" in the application's documents directory.
+//    NSLog(@"%@",[[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory  inDomains:NSUserDomainMask] lastObject]);
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
 
 - (NSManagedObjectModel *)managedObjectModel {
-    // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
@@ -66,26 +87,21 @@
 }
 
 - (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
-    // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it.
     if (_persistentStoreCoordinator != nil) {
         return _persistentStoreCoordinator;
     }
-    
-    // Create the coordinator and store
     
     _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
     NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"iScream.sqlite"];
     NSError *error = nil;
     NSString *failureReason = @"There was an error creating or loading the application's saved data.";
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
-        // Report any error we got.
+    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption, [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
+    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error]) {
         NSMutableDictionary *dict = [NSMutableDictionary dictionary];
         dict[NSLocalizedDescriptionKey] = @"Failed to initialize the application's saved data";
         dict[NSLocalizedFailureReasonErrorKey] = failureReason;
         dict[NSUnderlyingErrorKey] = error;
         error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
-        // Replace this with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
     }
@@ -95,7 +111,6 @@
 
 
 - (NSManagedObjectContext *)managedObjectContext {
-    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.)
     if (_managedObjectContext != nil) {
         return _managedObjectContext;
     }
@@ -116,8 +131,6 @@
     if (managedObjectContext != nil) {
         NSError *error = nil;
         if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
             abort();
         }
--- iScream/ViewController.m
+++ iScream/ViewController.m
@@ -7,21 +7,61 @@
 //
 
 #import "ViewController.h"
+#import <CoreData/CoreData.h>
+#import "AppDelegate.h"
+#import "Flavors.h"
+#import "InventoryItems.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) AppDelegate            *appDelegate;
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, strong) NSArray                *flavorsArray;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Core Methods
+
+- (NSArray *)fetchFlavors {
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Flavors" inManagedObjectContext:_managedObjectContext];
+    [fetchRequest setEntity:entity];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"flavorName" ascending:YES];
+    NSArray *sortDescriptors = [[NSArray alloc] initWithObjects:sortDescriptor, nil];
+    [fetchRequest setSortDescriptors:sortDescriptors];
+    
+    NSError *error;
+    NSArray *fetchResults = [_managedObjectContext executeFetchRequest:fetchRequest error:&error];
+    NSLog(@"Fetched %lu Flavors",(unsigned long)[fetchResults count]);
+
+    return [NSMutableArray arrayWithArray:fetchResults];
+}
+
+- (float)totalInventoryForFlavor:(Flavors *)flavor {
+    float totalInGallons = 0.0;
+    NSArray *flavorInventoryArray = [[flavor relationshipFlavorInventoryItems] allObjects];
+    for (InventoryItems *inventoryItem in flavorInventoryArray) {
+        totalInGallons = totalInGallons + [[inventoryItem sizeInGallons] floatValue];
+    }
+    return totalInGallons;
+}
+
+#pragma mark - Life Cycle Methods
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    _managedObjectContext = _appDelegate.managedObjectContext;
+    _flavorsArray = [self fetchFlavors];
+    for (Flavors *flavor in _flavorsArray) {
+        NSLog(@"Flavor: %@ Gallons: %.2f",[flavor flavorName],[self totalInventoryForFlavor:flavor]);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
